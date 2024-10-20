f4a1dbe6ed3555fb616e002af168ca1c7f9fcd20
6ukhwa <qwe1212485@gmail.com>
2024-10-20 19:18:33 +0900

    241020

--- portfolio004/js/js.js
+++ portfolio004/js/js.js
@@ -19,14 +19,27 @@ $(document).ready(function() {
     $('.search').on('click', function() {
 
         $('header').addClass('expanded');
+        $('.search_pop').addClass('on')
       });
     
     // close 클릭시 remove
     $('.close').on('click', function(){
 
+      $('nav').removeClass('on');
         $('header').removeClass('expanded')
+        $('.search_pop').removeClass('on')
     });
 
+    // nav를 호버할 때 header 높이 400px로 조절
+  $('nav').hover(function() {
+      $('header').addClass('expanded');
+      $('nav').addClass('on');
+      $('.search_pop').removeClass('on')
+    },
+    function() {
+      $('header').removeClass('expanded');
+    }
+  );
 
     // 마우스를 올렸을 때 애니메이션 실행
     $('.second li').on('mouseenter', function() {
@@ -36,10 +49,25 @@ $(document).ready(function() {
       });
   
       // 마우스를 뗐을 때 애니메이션 실행
-      $('li').on('mouseleave', function() {
+      $('.second li').on('mouseleave', function() {
         $(this).find('.cover').stop().animate({
           top: '85%'
         }, 600); // 0.8초에 걸쳐 다시 아래로 이동
       });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
   }); //-------------돈땃쥐----------------
